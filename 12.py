7d3692de5045e9519c10b52ef31c08c64f5f3c82
donzoru <donzoru@gmail.com>
2018-03-30 10:12:34 +0800

    12

---
+++ 12.py
@@ -0,0 +1,17 @@
+def calu(n):
+	t = int(n**0.5)
+	ans = int(1)
+	for i in range ( 2 , t ):
+		tem = int(1);
+		while( n%i == 0 ):
+			tem = tem + 1
+			n /= i
+		ans = ans*tem
+	return ans
+sum = int(0)
+for i in range ( 1 , 1000000 ):
+	sum = sum + int(i)
+	if( calu(sum) >= 500) :
+		print(sum);
+		break;
+		
